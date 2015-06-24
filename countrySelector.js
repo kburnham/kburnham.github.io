7754552932185a26cb5c801c738923c033003478
KEVIN R BURNHAM <kb@KEVINs-MacBook-Pro.local>
2015-06-24 10:33:08 -0500

    countriesToInclude variable generated via checkboxes

---
+++ countrySelector.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	
+	
+	d3.tsv("/muslim.attitudes.to.USA.China.tsv", function (data) {
+		var allCountries = dimple.getUniqueValues(data, "country");
+		for (i = 0; i < allCountries.length; i++) {
+			$('form').append('<input type="checkbox" name="' + allCountries[i] + '">' + allCountries[i] + '</input><br>');
+
+		}
+		$('form').append('<input type="submit" value="Make charts">');
+		countriesToInclude = [];
+		$(":checkbox").click(function () {
+			var index = jQuery.inArray(this.name, countriesToInclude);
+			if (index == -1) {
+				countriesToInclude.push(this.name);
+			} else {
+				countriesToInclude.splice(index, 1);
+			}
+			
+			console.log(countriesToInclude);
+		})
+	});
+
+
+		
+
+	
+	
+
+});
