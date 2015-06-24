c4d2c72ed6ee27dbb09edd17eb5809492d2358ca
KEVIN R BURNHAM <kb@KEVINs-MacBook-Pro.local>
2015-06-24 11:39:19 -0500

    add columns to country selector

--- countrySelector.js
+++ countrySelector.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 	
 	d3.tsv("/muslim.attitudes.to.USA.China.tsv", function (data) {
 		var allCountries = dimple.getUniqueValues(data, "country");
+		
 		for (i = 0; i < allCountries.length; i++) {
 			$('form').append('<input type="checkbox" name="' + allCountries[i] + '">' + allCountries[i] + '</input><br>');
 
